6773f61b698e76ea6ec03ec6baae4f4b3aee71bd
Grace Zhang <gracezhg@fb.com>
2021-06-24 12:30:56 -0700

    collections layout

---
+++ Flix/Views/MovieCollectionCell.h
@@ -0,0 +1,16 @@
+//
+//  MovieCollectionCell.h
+//  Flix
+//
+//  Created by gracezhg on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCollectionCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
